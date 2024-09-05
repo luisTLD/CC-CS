b21bbea7608670bab950570f2ab6bbae008328a2
Luis Toledo <141417786+luisTLD@users.noreply.github.com>
2024-09-05 13:28:52 -0300

    InsertionWithBinaryS

--- AED's/Fundamentos/Ordenacao/C/InsertionSort.h
+++ AED's/Fundamentos/Ordenacao/C/InsertionSort.h
@@ -1,19 +1,59 @@
 #ifndef InsertionSort_h
 #define InsertionSort_h
 
-// InsertionSort -> n^2
-void insertionSort(int arr[], int length)
+
+int binarySearchInsertion(int array[], int right, int key)
+{
+    int left = 0;
+    while (left <= right)
+    {
+        int middle = (left + right) / 2;
+        if (array[middle] == key)
+        {
+            return middle + 1;
+        }
+        else if (array[middle] > key)
+        {
+            right = middle - 1;
+        }
+        else 
+        {
+            left = middle + 1;
+        }
+    }
+    return left;
+}
+
+// InsertionSort with binary search (N log(N) comp / n^2 moves)
+void insertionSortOpt(int array[], int length)
 {
     for (int i = 1; i < length; i++)
     {
-        int tmp = arr[i];
+        int tmp = array[i];
         int j = i - 1;
-        while (j >= 0 && arr[j] > tmp)
+        int locate = binarySearchInsertion(array, i, array[i]);
+        while (j >= locate)
         {
-            arr[j + 1] = arr[j];
+            array[j + 1] = array[j];
             j = j - 1;
         }
-        arr[j + 1] = tmp;
+        array[j + 1] = tmp;
+    }
+}
+
+// InsertionSort -> n^2
+void insertionSort(int array[], int length)
+{
+    for (int i = 1; i < length; i++)
+    {
+        int tmp = array[i];
+        int j = i - 1;
+        while (j >= 0 && array[j] > tmp)
+        {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = tmp;
     }
 }
 
