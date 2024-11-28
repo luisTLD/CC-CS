323dd41f223c2b39da7d7a22e8539c289eceb8ee
Luis Toledo <141417786+luisTLD@users.noreply.github.com>
2024-11-28 16:03:58 -0300

    AVL C

--- AED's/Fundamentos/Estruturas de dados/C/AVL.h
+++ AED's/Fundamentos/Estruturas de dados/C/AVL.h
@@ -10,31 +10,31 @@ typedef struct Avl Avl;
     // Declaracoes das funcoes
 
 // Função para inicializar a árvore AVL
-void start(Avl* avl);
+void startAVL(Avl* avl);
 // Altura de um no
-int getHeight(Node_AVL* n);
+int getHeightAVL(Node_AVL* n);
 // Maior altura entre os 2 filhos de um no
-int maxSubHeight(Node_AVL* n);
+int maxSubHeightAVL(Node_AVL* n);
 // Fator balanceamento de um no
-int getBalance(Node_AVL* n);
+int getBalanceAVL(Node_AVL* n);
 // Printar arvore por altura
-void printLevelOrder(Avl* avl);
-void compactQueue(Node_AVL* queue[], int* start, int* end);
+void printLevelOrderAVL(Avl* avl);
+void compactQueueAVL(Node_AVL* queue[], int* start, int* end);
 // Funcao para balancear e retornar o no
-Node_AVL* balanceNode(Node_AVL* node);
+Node_AVL* balanceNodeAVL(Node_AVL* node);
 // Rotacao simples direita
-Node_AVL* rightRotate(Node_AVL* actual);
+Node_AVL* rightRotateAVL(Node_AVL* actual);
 // Rotacao simples esquerda
-Node_AVL* leftRotate(Node_AVL* actual);
+Node_AVL* leftRotateAVL(Node_AVL* actual);
 // Rotacao esquerda direita
-Node_AVL* leftRightRotate(Node_AVL* node);
+Node_AVL* leftRightRotateAVL(Node_AVL* node);
 // Rotacao direita esquerda
-Node_AVL* rightLeftRotate(Node_AVL* node);
+Node_AVL* rightLeftRotateAVL(Node_AVL* node);
 // Criar um novo no
-Node_AVL* newNode(int x);
+Node_AVL* newNodeAVL(int x);
 // Criar e inserir um novo no
-void insert(Avl* avl, int value);
-Node_AVL* insertCall(Node_AVL* node, int value);
+void insertAVL(Avl* avl, int value);
+Node_AVL* insertCallAVL(Node_AVL* node, int value);
 
 struct Avl 
 {
@@ -49,12 +49,12 @@ struct Node_AVL
     int height;
 };
 
-void start(Avl* avl)
+void startAVL(Avl* avl)
 {
     avl->root = NULL;
 }
 
-Node_AVL* newNode(int x)
+Node_AVL* newNodeAVL(int x)
 {
     Node_AVL* tmp = (Node_AVL*)malloc(sizeof(Node_AVL));
 
@@ -66,46 +66,46 @@ Node_AVL* newNode(int x)
     return tmp;
 }
 
-int getHeight(Node_AVL* n)
+int getHeightAVL(Node_AVL* n)
 {
     return n ? n->height : 0;
 }
 
-int maxSubHeight(Node_AVL* n)
+int maxSubHeightAVL(Node_AVL* n)
 {
-    int l = getHeight(n->left);
-    int r = getHeight(n->right);
+    int l = getHeightAVL(n->left);
+    int r = getHeightAVL(n->right);
     return l > r ? l : r;
 }
 
-int getBalance(Node_AVL* n)
+int getBalanceAVL(Node_AVL* n)
 {
-    return n ? getHeight(n->left) - getHeight(n->right) : 0;
+    return n ? getHeightAVL(n->left) - getHeightAVL(n->right) : 0;
 }
 
-Node_AVL* balanceNode(Node_AVL* node)
+Node_AVL* balanceNodeAVL(Node_AVL* node)
 {
-    int balance = getBalance(node);
-    int left_balance = getBalance(node->left);
-    int right_balance = getBalance(node->right);
+    int balance = getBalanceAVL(node);
+    int left_balance = getBalanceAVL(node->left);
+    int right_balance = getBalanceAVL(node->right);
 
     // Desbalanceamento a esquerda (rotação simples a direita)
-    if (balance > 1 && left_balance >= 0) return rightRotate(node);
+    if (balance > 1 && left_balance >= 0) return rightRotateAVL(node);
 
     // Desbalanceamento a direita (rotação simples a esquerda)
-    if (balance < -1 && right_balance <= 0) return leftRotate(node);
+    if (balance < -1 && right_balance <= 0) return leftRotateAVL(node);
 
     // Desbalanceamento a esquerda-direita (rotação dupla a esquerda)
-    if (balance > 1 && left_balance < 0) return leftRightRotate(node);
+    if (balance > 1 && left_balance < 0) return leftRightRotateAVL(node);
 
     // Desbalanceamento a direita-esquerda (rotação dupla a direita)
-    if (balance < -1 && right_balance > 0) return rightLeftRotate(node);
+    if (balance < -1 && right_balance > 0) return rightLeftRotateAVL(node);
 
     // Se o no estiver balanceado
     return node;
 }
 
-Node_AVL* rightRotate(Node_AVL* actual)
+Node_AVL* rightRotateAVL(Node_AVL* actual)
 {
     Node_AVL* actual_left = actual->left;         // O filho a esquerda de "actual"
     Node_AVL* actual_left_right = actual_left->right;    // O filho a direita de "left"
@@ -115,14 +115,14 @@ Node_AVL* rightRotate(Node_AVL* actual)
     actual->left = actual_left_right;
 
     // Atualizando as alturas
-    actual->height = 1 + maxSubHeight(actual);
-    actual_left->height = 1 + maxSubHeight(actual_left);
+    actual->height = 1 + maxSubHeightAVL(actual);
+    actual_left->height = 1 + maxSubHeightAVL(actual_left);
 
     // Retornando o novo no raiz
     return actual_left;
 }
 
-Node_AVL* leftRotate(Node_AVL* actual)
+Node_AVL* leftRotateAVL(Node_AVL* actual)
 {
     Node_AVL* actual_right = actual->right;        // O filho a direita de "actual"
     Node_AVL* actual_right_left = actual_right->left;     // O filho a esquerda de "right"
@@ -132,43 +132,43 @@ Node_AVL* leftRotate(Node_AVL* actual)
     actual->right = actual_right_left;
 
     // Atualizando as alturas
-    actual->height = 1 + maxSubHeight(actual);
-    actual_right->height = 1 + maxSubHeight(actual_right);
+    actual->height = 1 + maxSubHeightAVL(actual);
+    actual_right->height = 1 + maxSubHeightAVL(actual_right);
 
     // Retornando o novo no raiz
     return actual_right;
 }
 
-Node_AVL* leftRightRotate(Node_AVL* node)
+Node_AVL* leftRightRotateAVL(Node_AVL* node)
 {
-    node->left = leftRotate(node->left);
-    return rightRotate(node);
+    node->left = leftRotateAVL(node->left);
+    return rightRotateAVL(node);
 }
 
-Node_AVL* rightLeftRotate(Node_AVL* node)
+Node_AVL* rightLeftRotateAVL(Node_AVL* node)
 {
-    node->right = rightRotate(node->right);
-    return leftRotate(node);
+    node->right = rightRotateAVL(node->right);
+    return leftRotateAVL(node);
 }
 
-void insert(Avl* avl, int value)
+void insertAVL(Avl* avl, int value)
 {
-    avl->root = insertCall(avl->root, value);
+    avl->root = insertCallAVL(avl->root, value);
 }
 
-Node_AVL* insertCall(Node_AVL* node, int value)
+Node_AVL* insertCallAVL(Node_AVL* node, int value)
 {
-    if (!node) return newNode(value);  // Cria um novo no se o no atual for NULL
-    else if (value < node->value) node->left = insertCall(node->left, value); 
-    else if (value > node->value) node->right = insertCall(node->right, value);
+    if (!node) return newNodeAVL(value);  // Cria um novo no se o no atual for NULL
+    else if (value < node->value) node->left = insertCallAVL(node->left, value); 
+    else if (value > node->value) node->right = insertCallAVL(node->right, value);
     else return node;  // Nao permite duplicatas
 
-    node->height = 1 + maxSubHeight(node);
+    node->height = 1 + maxSubHeightAVL(node);
 
-    return balanceNode(node);  // Realiza o balanceamento apos a insercao
+    return balanceNodeAVL(node);  // Realiza o balanceamento apos a insercao
 }
 
-void printLevelOrder(Avl* avl)
+void printLevelOrderAVL(Avl* avl)
 {
     Node_AVL* queue[100];
 
@@ -190,7 +190,7 @@ void printLevelOrder(Avl* avl)
         printf("%d ", node->value);
 
         // Verifica se a fila esta quase cheia para reorganizar 
-        if (end > 96) compactQueue(queue, &start, &end);
+        if (end > 96) compactQueueAVL(queue, &start, &end);
 
         // Adicionar os filhos a lista
         if (node->left) queue[end++] = node->left;
@@ -198,7 +198,7 @@ void printLevelOrder(Avl* avl)
     }
 }
 
-void compactQueue(Node_AVL* queue[], int* start, int* end)
+void compactQueueAVL(Node_AVL* queue[], int* start, int* end)
 {
     int j = 0;
 
